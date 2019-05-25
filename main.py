24a56e5552f919ab6676aab905e25c7d02ae9eee
Matthew Benzar <benzarm@spu.edu>
2019-05-25 14:22:10 -0700

    Updated  with character choosing & attack options!

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def charSelect():
 	print("As you get up, the chains jangle, and you look down at yourself\n")
 
 	while(1):
+		global userCode
 		userClass = input("Are you be a:\nA. fighter\nB. wizard\nC. ranger?\n\n")
 
 		if(userClass == "A" or userClass == "a" or userClass == "fighter" or userClass == "Fighter"):
@@ -52,40 +53,76 @@ def environSelect(val):
 		print("Among the bloodthirsty fans and vendors trying to make a living, bets are tossed around and an armed opponent challenges you...\n")
 
 def userAction():
-	#fighter options
+	
 	while(1):
+		#fighter options
 		if(userCode == "A"):
 			#prompt
-			fighterAction = input("Will you:\nA. Slash?\nB. Roll?\nC. Charge?\n\n")
+			fighterAction = input("Act with a:\nA. Slash?\nB. Roll?\nC. Charge?\n\n")
 			#slash
-			if(fighterAction == "A" or userClass == "a" or userClass == "Slash" or userClass == "slash"):
+			if(fighterAction == "A" or fighterAction == "a" or fighterAction == "Slash" or fighterAction == "slash"):
+				print("SLASH wurk")
 				fighterCode = "A"
 				break
 			#roll
-			elif(userClass == "B" or userClass == "b" or userClass == "Roll" or userClass == "roll"):
+			elif(fighterAction == "B" or fighterAction == "b" or fighterAction == "Roll" or fighterAction == "roll"):
 				fighterCode = "B"
+				print("RICKROLL")
 				##NEGATE ALL DAMAGE? PERHAPS JUST DONT CHANGE THEIR HEALTH?
 				break
 			#charge
-			elif(userClass == "C" or userClass == "c" or userClass == "charge" or userClass == "charge"):
+			elif(fighterAction == "C" or fighterAction == "c" or fighterAction == "charge" or fighterAction == "charge"):
 				fighterCode = "C"
+				print("CHARGE WORKD")
 				#MAKE IT SO THAT THEY TAKE EXTRA DAMAGE
 				break
-			else:
-				print("The nerves hit and you take a second, time slows down and you choose to...\n")
 
-	# #wizard options
-	# elif(userCode == "B"):
-	# 	#attack spell
-	# 	#healing spell
-	# 	#
-	# #ranger options
-	# else:
-	# 	#fire arrow
-	# 	#dodge
-	# 	#normal arrow
+		#wizard options
+		elif(userCode == "B"):
+			#prompt
+			wizAction = input("Cast a(n):\nA. Attack spell?\nB. Healing spell?\nC. Curse spell?\n\n")
+			#attack spell
+			if(wizAction == "A" or wizAction == "a" or wizAction == "Attack Spell" or wizAction == "attack spell"):
+				wizCode = "A"
+				print("ATTACK WORK")
+				break
+			#healing spell
+			elif(wizAction == "B" or wizAction == "b" or wizAction == "Roll" or wizAction == "roll"):
+				wizCode = "B"
+				print("HEALING GOOD")
+				#restore health if past DC 15
+				break
+			#curse spell
+			elif(wizAction == "C" or wizAction == "c" or wizAction == "charge" or wizAction == "charge"):
+				wizCode = "C"
+				print("CURSE GOOD")
+				#DC 10 to hit, but if under 10, takes damage
+				break
 
-# 	#userAction = input("How will you attack?\n")
+		#ranger options
+		elif(userCode == "C"):
+			#prompt
+			rangeAction = input("Choose to:\nA. Shoot a normal arrow?\nB. Dodge?\nC. Shoot a fire arrow?\n\n")
+			#normal
+			if(rangeAction == "A" or rangeAction == "a" or rangeAction == "Shoot a normal arrow" or rangeAction == "shoot a normal arrow"):				
+				rangeCode = "A"
+				print("NORMAL ARROW")
+				break
+			#dodge
+			elif(rangeAction == "B" or rangeAction == "b" or rangeAction == "Dodge" or rangeAction == "dodge"):
+				rangeCode = "B"
+				print("DODGE")
+				#restore health if past DC 15
+				break
+			#fire 
+			elif(rangeAction == "C" or rangeAction == "c" or rangeAction == "Shoot a fire arrow" or rangeAction == "shoot a fire arrow"):
+				rangeCode = "C"
+				print("FIRE")
+				#DC 10 to hit, but if under 10, takes damage
+				break						
+		else:
+			print("The nerves hit and you take a second, time slows down and you choose to...\n")
+
 # 	#print(userAttack)
 
 welcome()
@@ -93,3 +130,4 @@ charSelect()
 #chooses the setting for the game
 environment = random.randint(0,5)
 environSelect(environment)
+userAction()
