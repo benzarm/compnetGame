33fbf544a84bf28c0c2cadd140bd70c0d73f0afb
trentcowden <37824478+trentcowden@users.noreply.github.com>
2019-05-25 14:36:33 -0700

    welcome messages and basic server functionality

--- testClient.py
+++ testClient.py
@@ -3,10 +3,17 @@ import sys
 import threading
 
 clientSocket = socket(AF_INET, SOCK_STREAM) #TCP socket
-serverPort = 4000
-clientSocket.connect(('10.1.10.175', serverPort))
+serverIP = str(sys.argv[1])
+serverPort = int(sys.argv[2])
+clientSocket.connect((serverIP, serverPort))
 print("Connected to server on port " + str(serverPort) + ". Say hello!")
 
-while(1):
-		sendMessage = input()
-		clientSocket.send(sendMessage)
+print("\n ~ Welcome to Trent & Matt's Wild [text] Adventure! ~")
+print("Waiting for both players to join...")
+ready = int(clientSocket.recv(1024))
+if (ready):
+	print("You find yourself in a prison, the door opens, and a gruff voice yells at you..")
+	userName = input("'Whats your name, prisoner?'\n\n")
+	print("\nOkay,", userName, "get out there and fight for your life\nBest of luck *wink*\n...\n")
+
+	
--- wildServer.py
+++ wildServer.py
@@ -3,13 +3,21 @@ import threading
 import sys
 import random
 
+
 serverSocket = socket(AF_INET, SOCK_STREAM) #TCP (reliable)
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) #make port reusable
-serverPort = 4000
-serverSocket.bind(('10.1.10.175', serverPort))
+serverIP = str(sys.argv[1])
+serverPort = int(sys.argv[2])
+serverSocket.bind((serverIP, serverPort))
 serverSocket.listen(1)
 print("Game ready to accept players on port " + str(serverPort))
 
-while(1):
-	conn, addr = serverSocket.accept()
-	print("New client connected from address: " + str(addr))
+#initial stuff
+print("Waiting to accept first player...")
+socket1, addr1 = serverSocket.accept()
+print("Player 1 joined, waiting to accept second player...")
+socket2, addr2 = serverSocket.accept()
+print("Player 2 joined, ready to begin!")
+
+socket1.send("1".encode('utf-8'))
+socket2.send("1".encode('utf-8'))
