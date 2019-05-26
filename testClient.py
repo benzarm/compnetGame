0a62048ba7693a32878e07e0fdc7ce4f17a5b1c5
trentcowden <37824478+trentcowden@users.noreply.github.com>
2019-05-25 18:30:06 -0700

    Removed test statements

--- testClient.py
+++ testClient.py
@@ -21,6 +21,7 @@ if (ready):
 	print("You find yourself in a prison, the door opens, and a gruff voice yells at you..")
 	userName = input("'Whats your name, prisoner?'\n\n")
 	print("\nOkay,", userName, "get out there and fight for your life\nBest of luck *wink*\n...\n")
+	health = 0
 
 	'''
 	CHARACTER SELECT
@@ -35,7 +36,7 @@ if (ready):
 			userCode = "A"
 			fighterWeapon = "sword"
 			fighterDMG = 3 #just adjust the random number generation for it to be easier to hit
-			fighterHealth = 50
+			health = 50
 			print("You feel your muscles strengthen and your hand clasps around the sword given to you...\n")
 			break
 
@@ -43,7 +44,7 @@ if (ready):
 			userCode = "B"
 			wizWeapon = "wand"
 			wizDMG = 5 #slightly harder to hit
-			wizHealth = 35
+			health = 35
 			print("Arcane energy rushes through your veins and at the touch of your wand, you come to your magical senses...\n")
 			break
 
@@ -51,11 +52,12 @@ if (ready):
 			userCode = "C"
 			rangerWeapon = "bow & arrow"
 			rangerDMG = 7 #hardest to hit
-			rangerHealth = 30
+			health = 30
 			print("As you test the string of your bow, you feel the energy of the bows thwang reverberate within you...\n")
 			break
 		else:
 			print("Your vision is foggy, and you can't quite see. As you rub your eyes you look down and see...\n")
+			
 	#send over character code		
 	clientSocket.send(userCode.encode('utf-8'))
 
@@ -86,19 +88,16 @@ if (ready):
 			fighterAction = input("Act with a:\nA. Slash?\nB. Roll?\nC. Charge?\n\n")
 			#slash
 			if(fighterAction == "A" or fighterAction == "a" or fighterAction == "Slash" or fighterAction == "slash"):
-				print("SLASH wurk")
 				attackCode = "A1"
 				break
 			#roll
 			elif(fighterAction == "B" or fighterAction == "b" or fighterAction == "Roll" or fighterAction == "roll"):
 				attackCode = "A2"
-				print("RICKROLL")
 				##NEGATE ALL DAMAGE? PERHAPS JUST DONT CHANGE THEIR HEALTH?
 				break
 			#charge
 			elif(fighterAction == "C" or fighterAction == "c" or fighterAction == "charge" or fighterAction == "charge"):
 				attackCode = "A3"
-				print("CHARGE WORKD")
 				#MAKE IT SO THAT THEY TAKE EXTRA DAMAGE
 				break
 
@@ -109,18 +108,15 @@ if (ready):
 			#attack spell
 			if(wizAction == "A" or wizAction == "a" or wizAction == "Attack Spell" or wizAction == "attack spell"):
 				attackCode = "B1"
-				print("ATTACK WORK")
 				break
 			#healing spell
 			elif(wizAction == "B" or wizAction == "b" or wizAction == "Roll" or wizAction == "roll"):
 				attackCode = "B2"
-				print("HEALING GOOD")
 				#restore health if past DC 15
 				break
 			#curse spell
 			elif(wizAction == "C" or wizAction == "c" or wizAction == "charge" or wizAction == "charge"):
 				attackCode = "B3"
-				print("CURSE GOOD")
 				#DC 10 to hit, but if under 10, takes damage
 				break
 
@@ -131,22 +127,21 @@ if (ready):
 			#normal
 			if(rangeAction == "A" or rangeAction == "a" or rangeAction == "Shoot a normal arrow" or rangeAction == "shoot a normal arrow"):				
 				attackCode = "C1"
-				print("NORMAL ARROW")
 				break
 			#dodge
 			elif(rangeAction == "B" or rangeAction == "b" or rangeAction == "Dodge" or rangeAction == "dodge"):
 				attackCode = "C2"
-				print("DODGE")
 				#restore health if past DC 15
 				break
 			#fire 
 			elif(rangeAction == "C" or rangeAction == "c" or rangeAction == "Shoot a fire arrow" or rangeAction == "shoot a fire arrow"):
 				attackCode = "C3"
-				print("FIRE")
 				#DC 10 to hit, but if under 10, takes damage
 				break						
 		else:
 			print("The nerves got to you and you selected the wrong option, try again...\n")
 
-	#send it
+	#send attack code
 	clientSocket.send(attackCode.encode('utf-8'))
+
+	
