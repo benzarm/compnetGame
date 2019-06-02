2b6fb18cbb57180017f4d837d2de11a9ec0035bd
trentcowden <37824478+trentcowden@users.noreply.github.com>
2019-06-01 22:43:47 -0700

    added sick music

--- wildClient.py
+++ wildClient.py
@@ -3,6 +3,7 @@ import sys
 import threading
 import random
 import time
+from pygame import mixer
 
 def print_slow(str):
 	for letter in str:
@@ -26,11 +27,17 @@ clientSocket.connect((serverIP, serverPort))
 #helpful message :) 
 print("Connected to server on port " + str(serverPort) + ". Say hello!")
 
+mixer.init()
+mixer.music.load("scape_main.mp3")
+mixer.music.play()
+
 #welcome message
 print_slow("\n ~ Welcome to Trent & Matt's Wild [text] Adventure! ~")
 print("\n")
 print_slow("Waiting for both players to join...")
 print("\n")
+
+
 
 '''
 INTIALIZING PREGAME/NAME-------------------------------------------------------
@@ -68,27 +75,27 @@ while(1):
 		userCode = "A"
 		fighterWeapon = "sword"
 		fighterDMG = 3 #just adjust the random number generation for it to be easier to hit
-		health = 50
-		print("You feel your muscles strengthen and your hand clasps around the sword given to you...\n")
+		health = 40
+		print_slow("You feel your muscles strengthen and your hand clasps around the sword given to you...\n")
 		break
 
 	elif(userClass == "B" or userClass == "b" or userClass == "wizard" or userClass == "Wizard"):
 		userCode = "B"
 		wizWeapon = "wand"
 		wizDMG = 5 #slightly harder to hit
-		health = 35
-		print("Arcane energy rushes through your veins and at the touch of your wand, you come to your magical senses...\n")
+		health = 30
+		print_slow("Arcane energy rushes through your veins and at the touch of your wand, you come to your magical senses...\n")
 		break
 
 	elif(userClass == "C" or userClass == "c" or userClass == "ranger" or userClass == "Ranger"):
 		userCode = "C"
 		rangerWeapon = "bow & arrow"
 		rangerDMG = 7 #hardest to hit
-		health = 30
-		print("As you test the string of your bow, you feel the energy of the bows thwang reverberate within you...\n")
+		health = 25
+		print_slow("As you test the string of your bow, you feel the energy of the bows thwang reverberate within you...\n")
 		break
 	else:
-		print("Your vision is foggy, and you can't quite see. As you rub your eyes you look down and see...\n")
+		print_slow("Your vision is foggy, and you can't quite see. As you rub your eyes you look down and see...\n")
 		
 #send over character code to server for processing	
 clientSocket.send(userCode.encode('utf-8'))
@@ -96,16 +103,16 @@ clientSocket.send(userCode.encode('utf-8'))
 '''
 ENVIRONMENT SELECT-------------------------------------------------------------
 '''
-environment = random.randint(0,5)
+environment = random.randint(1,3)
 
 if(environment == 1):
-	print_slow("And as the door opens, you are nearly blinded with the sharp reflection of the sun on a sea of sand.")
+	print_slow("And as the door opens, you are nearly blinded with the sharp reflection of the sun on a sea of sand. ")
 	print_slow("The heat is draining, but in the distance an armed opponent is walking towards you...\n") 
 elif(environment == 2):
-	print_slow("And as the door opens, the superficial torch light becomes the brightest beacon in a dull forest.")
-	print("As the wind howls and you see red eyes blink in the distance, a figure, with weapon in hand approaches...\n")
+	print_slow("And as the door opens, the superficial torch light becomes the brightest beacon in a dull forest. ")
+	print_slow("As the wind howls and you see red eyes blink in the distance, a figure, with weapon in hand approaches...\n")
 else:
-	print_slow("And as the door slams open, a deafening roar of shouts and screams and song from a crowd cheer at your potential demise.")
+	print_slow("And as the door slams open, a deafening roar of shouts and screams and song from a crowd cheer at your potential demise. ")
 	print_slow("Among the bloodthirsty fans and vendors trying to make a living, bets are tossed around and an armed opponent challenges you...\n")
 
 '''
@@ -195,7 +202,7 @@ while(1):
 	if (modifier == 1):
 		damage = 0
 	elif (modifier == 2):
-		health += 5
+		health += 3
 	elif (modifier == 3):
 		damage -= 5
 		if (damage < 0):
@@ -219,7 +226,12 @@ while(1):
 
 	#these message signify the end of the game, so we can close the socket
 	#and exit the loop once we receive one of these
-	if (healthMessage == "You win!" or healthMessage == "You lose!"):
+	if (healthMessage == "You win!" or healthMessage == "You lose!" or healthMessage == "Game over, it's a draw!"):
+		mixer.music.stop()
+		mixer.music.load("gnome_theme.mp3")
+		mixer.music.play()
+		print_slow("Game ending in 15 seconds...")
+		time.sleep(15)
 		clientSocket.close()
 		break
 
